1b2859906480e98db8094e43d23ce5f89ed64b3d
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-05-25 10:11:34 -0300

    Refatorando as seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Coin.create!(
+puts "Criando Moedas..."
+
+coins = [
+  {    
     description: "Bitcoin",
     acronym: "BTC",
     url_image: "https://logosmarcas.net/wp-content/uploads/2020/08/Bitcoin-Logo.png"
-)
-
-Coin.create!(
+  },
+  {
     description: "Etherium",
     acronym: "ETH",
     url_image: "https://marcas-logos.net/wp-content/uploads/2020/03/ETHEREUM-LOGO.png"
-)
+  },
+]
 
-Coin.create!(
-    description: "Dash",
-    acronym: "DASH",
-    url_image: "https://s2.coinmarketcap.com/static/img/coins/200x200/131.png"
-)
+coins.each do |coin|
+  Coin.find_or_create_by!(coin)
+end
+
+puts "Moedas Criadas com sucesso!"
