aca6fdbf6438411508bee1acfe84bd58dbb0418f
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-05-31 11:01:34 -0300

    Select no padrão MVC

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,5 +1,6 @@
 class CoinsController < ApplicationController
   before_action :set_coin, only: %i[ show edit update destroy ]
+  before_action :set_mining_type_options, only: [:new, :edit, :update, :create]
 
   # GET /coins or /coins.json
   def index
@@ -58,7 +59,12 @@ class CoinsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
+    def set_mining_type_options
+      @mining_type_options = MiningType.all.pluck(:name, :id)
+    end
+  
+      # Use callbacks to share common setup or constraints between actions.
     def set_coin
       @coin = Coin.find(params[:id])
     end
