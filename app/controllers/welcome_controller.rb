6484efdcaea096a2dc883459c055048670bf8e8a
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-06-01 08:58:32 -0300

    Session and Cookies

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:aula] = "Aula de ruby on Rails - Carlos Santos [COOKIES]"
+    session[:curso] = "Curso de ruby on Rails - Carlos Santos [Session]"
+
     @nome = params[:nome]
   end
 end
